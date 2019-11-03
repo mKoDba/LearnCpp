7b99790dd88e67dd39197639cf7f7c1d6e0b5945
Mario <Mario@DESKTOP-12A3G8V>
2019-11-03 23:59:30 +0100

    full day6 solution

--- day6.cpp
+++ day6.cpp
@@ -14,10 +14,10 @@
 #include <map>
 #include <ctime>
 #include <algorithm>
-#include <boost/algorithm/string.hpp>
 
 //using namespace std;
 #define FILE_LOCATION "C:\\Users\\Mario\\Desktop\\aoc2018\\day6.txt"
+#define MAX_TOTAL_DIST 10000
 
 struct Coord {
 	int x;
@@ -38,7 +38,7 @@ bool comp_fn(const std::pair<Coord, int>& lhs, const std::pair<Coord, int>& rhs)
 
 bool prepare(std::vector<Coord>& loc, int& max_x, int& max_y);
 int dist(Coord A, Coord B);
-int find_largest_area(std::vector<Coord>& loc, int& max_x, int& max_y);
+void find_largest_area(std::vector<Coord>& loc, int& max_x, int& max_y);
 
 int main(void){
     clock_t start = clock();
@@ -50,12 +50,69 @@ int main(void){
 		return EXIT_FAILURE;
 	}
 
-	int area = find_largest_area(loc, max_x, max_y);
-	//std::cout<<"try abs:"<<dist(Coord{9,9}, Coord{8,9})<<std::endl;
+	find_largest_area(loc, max_x, max_y);
 
     std::cout<<"\nduration: "<<float( clock () - start ) * 1000.0 /  CLOCKS_PER_SEC <<" ms"<<std::endl;
-    std::cout<<"Part 1: size of the largest (finite) area: "<<area<<std::endl;
+
 	return 0;
+}
+
+void find_largest_area(std::vector<Coord>& loc, int& max_x, int& max_y){
+	std::map<Coord, int> area;
+	std::vector<Coord> edge;
+	int region_size = 0;
+	Coord None = {-1,-1};
+
+	for(int i=0; i<=max_x; ++i){
+		for(int j=0; j<=max_y; ++j){
+			Coord C = {i,j};
+			Coord P_closest = {-1,-1};
+			int min = 10000;
+			int total_dist = 0;
+
+			for(Coord P: loc){
+				int m_dist = dist(C,P);
+				total_dist += m_dist;
+
+				if(m_dist < min){
+					min = m_dist;
+					P_closest = P;
+				}
+				else if(m_dist == min){
+					//min = -1;
+					P_closest = None;
+				}
+			}
+
+			if(total_dist < MAX_TOTAL_DIST){
+				region_size++;
+			}
+
+			if(i==0 || i==max_x || j==0 || j==max_y){
+				if(std::find(edge.begin(), edge.end(), P_closest) == edge.end()){
+					edge.push_back(P_closest);
+				}
+			}
+			area[P_closest]++;
+		}
+	}
+	/* remove coordinates tied with others and points
+	 * that have edge coordinates since they extend
+	 * to infinity */
+	for(auto P: edge){
+		//std::cout<<P.x<<" "<<P.y<<std::endl;
+		area.erase(P);
+	}
+	/*
+	for(auto it = area.cbegin(); it != area.cend(); ++it){
+		std::cout<<"("<<it->first.x<<", "<<it->first.y<<") "<<it->second<<std::endl;
+	}
+	std::cout<<"-------"<<std::endl;
+	*/
+
+	int res = std::max_element(area.begin(), area.end(), comp_fn)->second;
+    std::cout<<"Part 1: size of the largest (finite) area: "<<res<<std::endl;
+    std::cout<<"Part 2: region size with total distance < 10000: "<<region_size<<std::endl;
 }
 
 bool prepare(std::vector<Coord>& loc, int& max_x, int& max_y){
@@ -90,52 +147,3 @@ int dist(Coord A, Coord B){
 	return (std::abs(A.x-B.x)+std::abs(A.y-B.y));
 }
 
-
-int find_largest_area(std::vector<Coord>& loc, int& max_x, int& max_y){
-	std::map<Coord, int> area;
-	std::vector<Coord> edge;
-	Coord None = {-1,-1};
-
-	for(int i=0; i<=max_x; ++i){
-		for(int j=0; j<=max_y; ++j){
-			Coord C = {i,j};
-			Coord P_closest = {-1,-1};
-			int min = 10000;
-
-			for(Coord P: loc){
-				int m_dist = dist(C,P);
-				//std::cout<<"("<<P.x<<", "<<P.y<<")"<<"\n";
-				if(m_dist < min){
-					min = m_dist;
-					P_closest = P;
-				}
-				else if(m_dist == min){
-					//min = -1;
-					P_closest = None;
-				}
-			}
-
-			if(i==0 || i==max_x || j==0 || j==max_y){
-				if(std::find(edge.begin(), edge.end(), P_closest) == edge.end()){
-					edge.push_back(P_closest);
-				}
-			}
-			area[P_closest]++;
-		}
-	}
-	/* remove coordinates tied with others and points
-	 * that have edge coordinates since they extend
-	 * to infinity */
-	for(auto P: edge){
-		//std::cout<<P.x<<" "<<P.y<<std::endl;
-		area.erase(P);
-	}
-
-	for(auto it = area.cbegin(); it != area.cend(); ++it){
-		std::cout<<"("<<it->first.x<<", "<<it->first.y<<") "<<it->second<<std::endl;
-	}
-	std::cout<<"-------"<<std::endl;
-	int res = std::max_element(area.begin(), area.end(), comp_fn)->second;
-
-	return res;
-}
